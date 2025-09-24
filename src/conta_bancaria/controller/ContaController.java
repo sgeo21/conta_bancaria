d46b4905e27baf46ea57ea9f1f75a3420b86f786
Geovana <s.geo2112@gmail.com>
2025-09-24 19:46:34 -0300

    Inicio interface interna

---
+++ src/conta_bancaria/controller/ContaController.java
@@ -0,0 +1,59 @@
+package conta_bancaria.controller;
+
+import conta_bancaria.model.Conta;
+import conta_bancaria.repository.ContaRepository;
+
+public class ContaController implements ContaRepository{ // vai reclamar no ContaController
+	//e vai vamos trazer ele através da primeira sugestão, ->addunimplementedmetods.
+
+	@Override
+	public void listarTodas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cadastrar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Conta conta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void procurarPorNumero(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sacar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void depositar(int numero, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
---
+++ src/conta_bancaria/repository/ContaRepository.java
@@ -0,0 +1,23 @@
+package conta_bancaria.repository;
+
+import conta_bancaria.model.Conta;
+
+public interface ContaRepository {
+
+	//Métodos do CRUD
+	//métodods de consulta ( primeiras opções dp menu)
+	public void listarTodas(); //1
+	public void cadastrar(Conta conta); //2
+	public void atualizar(Conta conta); //3
+	public void procurarPorNumero (int numero); //4
+	public void deletar(int numero); //5
+	
+	//Metodos Bancários, fazer as operações bancárias
+	public void sacar(int numero, float valor); //6
+	public void depositar(int numero, float valor); //7
+	public void transferir(int numeroOrigem, int numeroDestino, float valor); //8
+	
+	//foram criados então precisam ser implementados
+	//criar uma classe de controle - class controler
+	
+}
